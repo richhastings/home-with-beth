79ea4ac3eccf4c4755182c3f7f930cd9843e992e
Rich Hastings <rich.hastings@clock.co.uk>
2022-05-15 18:25:39 +0100

    Set up tailwind

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -4,8 +4,8 @@ import client from '../client'
 
 const Index = ({posts}) => {
     return (
-      <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', minHeight: 'calc(100vh - 16px)'}}>
-        <h1 style={{fontFamily: 'sans-serif', textAlign: 'center'}}>Home with Beth - Coming Summer 2022</h1>
+      <div>
+        <h1 className="text-3xl font-bold underline">Home with Beth - Coming Summer 2022</h1>
         {/* {posts.length > 0 && posts.map(
           ({ _id, title = '', slug = '', publishedAt = '' }) =>
             slug && (
