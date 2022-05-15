c12bf46ffc48cbb1decc63918af8c660f1cfd897
Rich Hastings <rich.hastings@clock.co.uk>
2022-05-15 17:38:14 +0100

    Debugging

--- web/client.js
+++ web/client.js
@@ -3,6 +3,6 @@ const sanityClient = require('@sanity/client')
 module.exports = sanityClient({
   projectId: '6mrehaoy', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: false ,// `false` if you want to ensure fresh data
+  useCdn: true ,// `false` if you want to ensure fresh data
   apiVersion: '2021-08-31' 
 })
--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -53,7 +53,7 @@ export async function getStaticPaths() {
 
   return {
     paths: paths.map((slug) => ({params: {slug}})),
-    fallback: true,
+    fallback: false,
   }
 }
 
