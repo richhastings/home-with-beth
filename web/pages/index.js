434d2c378187fd8835a3b98f20c5384c8a2cd0bb
Rich Hastings <rich.hastings@clock.co.uk>
2022-05-15 16:31:10 +0100

    initial commit

---
+++ web/pages/index.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../client'
+
+const Index = ({posts}) => {
+    return (
+      <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', minHeight: 'calc(100vh - 16px)'}}>
+        <h1 style={{fontFamily: 'sans-serif', textAlign: 'center'}}>Home with Beth - Coming Summer 2022</h1>
+        {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/post/[slug]" as={`/post/${slug.current}`}>
+                  <a>{title}</a>
+                </Link>{' '}
+              </li>
+            )
+        )}
+      </div>
+    )
+}
+
+export async function getStaticProps() {
+    const posts = await client.fetch(groq`
+      *[_type == "post"] | order(publishedAt desc)
+    `)
+    return {
+      props: {
+        posts
+      }
+    }
+}
+
+export default Index
---
+++ web/pages/post/[slug].js
@@ -0,0 +1,71 @@
+import groq from 'groq'
+import client from '../../client'
+import { PortableText } from '@portabletext/react'
+import imageUrlBuilder from '@sanity/image-url'
+
+function urlFor (source) {
+  return imageUrlBuilder(client).image(source)
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img
+          alt={value.alt || ' '}
+          loading="lazy"
+          src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+        />
+      )
+    }
+  }
+}
+
+const Post = ({post}) => {
+  const {title, categories, body = []} = post
+
+  return (
+    <article>
+      <h1>{post?.slug?.current}</h1>
+      <p>{title}</p>
+      <div>{categories.map(category => category)}</div>
+      <PortableText
+        value={body}
+        components={ptComponents}
+      />
+    </article>
+  )
+}
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "categories": categories[]->title,
+  body
+}`
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(query, { slug })
+  return {
+    props: {
+      post
+    }
+  }
+}
+
+export default Post
