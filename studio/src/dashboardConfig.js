31cb1f32e3065af2bfeff674fec788bdedad0cba
Rich Hastings <rich.hastings@clock.co.uk>
2022-05-16 08:31:45 +0100

    Add netlify build integration

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Website',
+            apiId: 'c864ee5e-f044-4474-8332-bcb4241d2446',
+            buildHookId: '6281fd9c5aa37a586751be59',
+            name: 'home-with-beth',
+          }
+        ]
+      }
+    }
+  ]
+}
