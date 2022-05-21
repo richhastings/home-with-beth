170d534ad8957437a55d9fdc3bc29fb691ff6810
Rich Hastings <rich.hastings@clock.co.uk>
2022-05-21 13:22:47 +0100

    Update instagram function

--- web/netlify/functions/instagram.js
+++ web/netlify/functions/instagram.js
@@ -1,6 +1,6 @@
 require('isomorphic-fetch')
 
-const url = `https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables={"user_id":"847792", "first": 6}`
+const url = `https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables={"user_id":"847792","first":6}`
 
 const getPosts = async () => {
   const data = await fetch(url).then((res) => res.json())
@@ -11,6 +11,9 @@ exports.handler = async function (event, context) {
   const posts = await getPosts()
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(posts),
   }
 }
