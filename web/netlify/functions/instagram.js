71c48fc1e13b79ebf3a49261f8e34a7c5514b5d9
Rich Hastings <rich.hastings@clock.co.uk>
2022-05-21 11:51:00 +0100

    Fetch instagram posts

--- web/netlify/functions/instagram.js
+++ web/netlify/functions/instagram.js
@@ -1,6 +1,16 @@
+require('isomorphic-fetch')
+
+const url = `https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables={"user_id":"847792", "first": 6}`
+
+const getPosts = async () => {
+  const data = await fetch(url).then((res) => res.json())
+  return data
+}
+
 exports.handler = async function (event, context) {
+  const posts = await getPosts()
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello Instagram!' }),
+    body: JSON.stringify(posts),
   }
 }
