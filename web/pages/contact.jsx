1c532966a71395c4138d9945ac3630565d229ad0
Rich Hastings <rich.hastings@clock.co.uk>
2022-08-14 17:43:23 +0100

    Reinstate original form

--- web/pages/contact.jsx
+++ web/pages/contact.jsx
@@ -8,7 +8,6 @@ import Field from '../components/Field'
 import { contactPageQuery } from '../data/queries'
 import { PortableText } from '@portabletext/react'
 import { NextSeo } from 'next-seo'
-import { NetlifyForm } from 'react-netlify-forms'
 
 const Index = ({ data }) => {
   const { allLockup } = data
@@ -25,60 +24,24 @@ const Index = ({ data }) => {
           </div>
           <div>
             <div className="mt-12">
-              <NetlifyForm name="enquiry" action="/thank-you">
-                {({ handleChange, success, error }) => (
-                  <>
-                    <Field
-                      name="firstname"
-                      label="First name"
-                      handleChange={handleChange}
-                    />
-                    <Field
-                      name="lastname"
-                      label="Last name"
-                      handleChange={handleChange}
-                    />
-                    <Field
-                      type="email"
-                      name="email"
-                      label="Email address"
-                      handleChange={handleChange}
-                    />
-                    <Field
-                      type="select"
-                      name="product"
-                      label="Enquiry"
-                      handleChange={handleChange}
-                    />
-                    <Field
-                      type="textarea"
-                      name="message"
-                      label="Message"
-                      handleChange={handleChange}
-                    />
-
-                    <div className="col-span-3 text-center">
-                      <Button type="submit">Submit</Button>
-                    </div>
-                  </>
-                )}
-              </NetlifyForm>
-              {/* <form
+              <form
                 action="/thank-you"
                 name="enquiry"
                 method="POST"
                 className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-                netlify
+                data-netlify="true"
               >
                 <input type="hidden" name="form-name" value="enquiry" />
-                <input
-                  type="text"
-                  name={'first-name'}
-                  id={'last-name'}
-                  className="block w-full rounded border-lightgrey p-3 px-4 font-body"
-                />
-                
-              </form> */}
+                <Field name="firstname" label="First name" />
+                <Field name="last-name" label="Last name" />
+                <Field type="email" name="email" label="Email address" />
+                <Field type="select" name="product" label="Enquiry" />
+                <Field type="textarea" name="message" label="Message" />
+
+                <div className="col-span-3 text-center">
+                  <Button type="submit">Submit</Button>
+                </div>
+              </form>
             </div>
           </div>
         </Container>
