bb75e6380600fbd7f9318aeb148a99d7469e9e9c
Rich Hastings <rich.hastings@pentagonplay.co.uk>
2026-02-13 21:12:07 +0000

    Fix typo

--- web/pages/thank-you.jsx
+++ web/pages/thank-you.jsx
@@ -2,12 +2,12 @@ import { addApolloState, initializeApollo } from '../data/apollo'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { contactPageQuery } from '../data/queries'
-import Button from '../component  s/Button'
+import Button from '../components/Button'
 
 const Index = () => {
   return (
     <Layout hero={<Hero title="Thank you" short />} noHype>
-      <div className="overflow-hidden bg-white h-full">
+      <div className="h-full overflow-hidden bg-white">
         <div className="relative mx-auto max-w-3xl text-center">
           <div className="prose mb-8 max-w-none px-4 font-alt text-3xl">
             Thank you for contacting me, I'll be in touch soon!
